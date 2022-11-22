9382ade07e237ccf8558e30d30658256c39e66f0
mitchell <mitchell.s.obrien@gmail.com>
2022-11-22 18:12:37 +1100

    Moved JS script tage to bottom of HTML. Started JS logic to add a class value of either player 1 or 2 onto the divs and change the background to reflect choice.

---
+++ script.js
@@ -0,0 +1,22 @@
+//code to change the individual div boxes via adding an event listener to the parent
+//target each box via: document.querySelector('.bN1')  //use methods after ) such as ).textContent = " "
+
+let allSquares = document.querySelector('.gameContainer')
+let whoTurn = 1
+
+allSquares.addEventListener('click', function (event) {
+    let eachSquare = event.target
+    if (whoTurn % 2 === 0) {
+        eachSquare.classList.add('addPlayer2')
+        whoTurn++
+        console.log(whoTurn)
+    } else {
+        eachSquare.classList.add('addPlayer1')
+        whoTurn++
+        console.log(whoTurn)
+    }
+})
+
+// document.querySelector('.bN1').classList.add('addPlayer1')
+
+// document.querySelector('.bN2').classList.add('addPlayer2')
